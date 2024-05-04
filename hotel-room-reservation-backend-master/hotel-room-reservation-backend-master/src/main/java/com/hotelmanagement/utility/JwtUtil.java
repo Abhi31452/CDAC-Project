8f14e4fbbc773ec1c9d0f86c5b0f847ed4f204c0
Abhishek Auti <104745389+Abhi31452@users.noreply.github.com>
2024-05-04 16:29:45 +0530

    Update JwtUtil.java

--- hotel-room-reservation-backend-master/hotel-room-reservation-backend-master/src/main/java/com/hotelmanagement/utility/JwtUtil.java
+++ hotel-room-reservation-backend-master/hotel-room-reservation-backend-master/src/main/java/com/hotelmanagement/utility/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String secret = "codewithmurad";
+    private String secret = "Abhishek";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
